9f311378a5a5fe39dd42fc371d6e0604a239e4c8
dandreiolteanu <olteanuandrei@Olteanus-MacBook-Pro.local>
2017-03-28 11:19:50 +0300

    Added a ViewController

--- pocketMath/ViewController.swift
+++ pocketMath/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonSub1Constraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
