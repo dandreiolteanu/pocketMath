95989629c549e6150f2281cd8b322860fd24e5b3
dandreiolteanu <olteanuandrei@Olteanus-MacBook-Pro.local>
2017-03-29 02:11:09 +0300

    After Effects done, launch screen
    v01

--- pocketMath/ViewController.swift
+++ pocketMath/ViewController.swift
@@ -8,40 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    @IBOutlet weak var buttonSub1Constraint: NSLayoutConstraint!
+
+
+
+
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+    
+    @IBOutlet weak var pocketMathAnimated: UIImageView!
+    
+    
+
+//    @IBOutlet weak var buttonSub1Constraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttonSub1Constraint.constant -= view.bounds.width
+//       buttonSub1Constraint.constant -= view.bounds.width
+//        if !animationPerformedOnce {
+//            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+//                self.buttonSub1Constraint.constant += self.view.bounds.width
+//                self.view.layoutIfNeeded()
+//            }, completion: nil)
+//            
+//            //            UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveEaseOut, animations: {
+//            //                self.button2Constraint.constant += self.view.bounds.width
+//            //                self.view.layoutIfNeeded()
+//            //            }, completion: nil)
+//            
+//            animationPerformedOnce = true
+//        }
+//   
+        pocketMathAnimated.loadGif(name:"pocketMathAnimated")
         
+        _ = Timer.scheduledTimer(timeInterval: 0.85, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)
+
         
     }
     
     
-    var animationPerformedOnce = false
+    
+//    Function to return to the Home viewController after 1 second
+//
+//    _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)  -----> called in viewDidLoad()
+//
+    
+    
+    
+    func timeToMoveOn() {
+        self.performSegue(withIdentifier: "goHome", sender: self)
+    }
+    
+    
+    
+    
+//    var animationPerformedOnce = false
+    
+    
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        if !animationPerformedOnce {
-            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
-                self.buttonSub1Constraint.constant += self.view.bounds.width
-                self.view.layoutIfNeeded()
-            }, completion: nil)
-            
-//            UIView.animate(withDuration: 0.5, delay: 0.3, options: .curveEaseOut, animations: {
-//                self.button2Constraint.constant += self.view.bounds.width
-//                self.view.layoutIfNeeded()
-//            }, completion: nil)
-            
-            animationPerformedOnce = true
-        }
-        
         
         
     }
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
